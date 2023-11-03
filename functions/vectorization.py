1d5834bec9a715bf68589ac660ed7b2d8e799580
nourtheguy <nour.zeineddine11@gmail.com>
2023-11-03 19:01:06 +0200

    vectorization is done

--- functions/vectorization.py
+++ functions/vectorization.py
@@ -1,9 +1,8 @@
-import xml.etree.ElementTree as ET
-import math
-from preprocessing import *
-from pprint import pprint
-
-
+# Importing necessary libraries and modules
+import xml.etree.ElementTree as ET  # For XML parsing
+import math  # For mathematical operations
+from preprocessing import *  # Importing all functions from preprocessing module
+from pprint import pprint  # For pretty printing
 
 # Wd(ti) = TF(ti, D) * IDF(ti, C)
 # TF = frequency
@@ -17,109 +16,117 @@ from pprint import pprint
 # term context model
 # the key is a tuple (term, path) and the value is the TF
 # another method will be used if IDF is needed
+
+
+# Function to get the vector with term frequency (TF) for XML data
 def getVectorWithTF(root, path, vector):
-    path = path + "/" + root.tag
-    attributeDict = root.attrib
+    path = path + "/" + root.tag  # Constructing the path with the current XML tag
+    attributeDict = root.attrib  # Getting attributes of the current XML tag
 
-    # handling the tags
-    tagPath = path
-    wordlist = normalize_text(root.tag)
-    for word in wordlist:
-        dimension = (word, tagPath)
-        if dimension in vector:
-            vector[dimension] = vector[dimension] + 1
-        else:
-            vector[dimension] = 1
+    # Handling the current tag, its text, and its attributes
+    _handle_tags(root.tag, path, vector)
+    _handle_text(root.text, path, vector)
+    _handle_attributes(attributeDict, path, vector)
 
-    # handling the text
-    if root.text:
-        wordPath = path + "/#"
-        wordlist = normalize_text(root.text)
-        for word in wordlist:
-            dimension = (word, wordPath)
-            if dimension in vector:
-                vector[dimension] = vector[dimension] + 1
-            else:
-                vector[dimension] = 1
-
-    # handling the attribute
-    for att in attributeDict:
-        attributePath = path + "/" + att + "/@"
-        wordlist = normalize_text(attributeDict[att])
-        for word in wordlist:
-            dimension = (word, attributePath)
-            if dimension in vector:
-                vector[dimension] = vector[dimension] + 1
-            else:
-                vector[dimension] = 1
-
+    # Recursively processing child tags
     for child in root:
         getVectorWithTF(child, path, vector)
 
 
-# the following method will return the vector with only TF for only text
-# it will be used to get the vector of text query
-# we will assume the path is an empty string in this case
+# Function to handle XML tags and update the vector
+def _handle_tags(tag, path, vector):
+    tagPath = path  # Path for the current tag
+    wordlist = normalize_text(tag)  # Normalizing the tag text
+    _update_vector(
+        wordlist, tagPath, vector
+    )  # Updating the vector with normalized words
+
+
+# Function to handle text inside XML tags and update the vector
+def _handle_text(text, path, vector):
+    if text:  # Check if text is not empty
+        wordPath = path + "/#"  # Constructing path for the text
+        wordlist = normalize_text(text)  # Normalizing the text
+        _update_vector(
+            wordlist, wordPath, vector
+        )  # Updating the vector with normalized words
+    else:
+        print("not text")
+
+
+# Function to handle attributes of XML tags and update the vector
+def _handle_attributes(attributeDict, path, vector):
+    for att in attributeDict:  # Iterating through each attribute
+        attributePath = path + "/" + att + "/@"  # Constructing path for the attribute
+        wordlist = normalize_text(attributeDict[att])  # Normalizing the attribute value
+        _update_vector(
+            wordlist, attributePath, vector
+        )  # Updating the vector with normalized words
+
+
+# Function to update the vector with normalized words
+def _update_vector(wordlist, path, vector):
+    for word in wordlist:  # Iterating through each word in the wordlist
+        dimension = (word, path)  # Creating a tuple of word and its path
+        # Incrementing the count of the word in the vector or initializing it to 1 if not present
+        vector[dimension] = vector.get(dimension, 0) + 1
+
+
+# Function to get the vector for a text query
 def getTextQueryVector(text):
-    vector = {}
-    wordlist = normalize_text(text)
-
-    for word in wordlist:
-        dimension = (word, "")
-        if dimension in vector:
-            vector[dimension] = vector[dimension] + 1
-        else:
-            vector[dimension] = 1
-    return vector
+    vector = {}  # Initializing an empty vector
+    wordlist = normalize_text(text)  # Normalizing the query text
+    _update_vector(wordlist, "", vector)  # Updating the vector with normalized words
+    return vector  # Returning the vector
 
 
-# the following method will compute the DF of a specific dimension (term, path)
-# the list of vectors contains only TF at this stage
+# Function to calculate document frequency (DF) for a dimension
 def DF(dimension, listOfVectors):
-    DF = 0
-    for path in listOfVectors:
-        vector = listOfVectors[path]
-        if dimension in vector:
-            DF = DF + 1
-    return DF
+    # Counting the number of vectors containing the dimension
+    return sum(1 for vector in listOfVectors.values() if dimension in vector)
 
 
-# in this method we will take the array of vector with TF
-# and will add IDF to the weight
-# this method will use the DF method
+# Function to calculate inverse document frequency (IDF) for all vectors
 def IDF(listOfVectors):
-    for path in listOfVectors:
-        vector = listOfVectors[path]
-        for dimension in vector:
-            vector[dimension] = vector[dimension] * math.log(
+    for vector in listOfVectors.values():  # Iterating through each vector
+        for dimension in vector:  # Iterating through each dimension in the vector
+            # Multiplying the term frequency with the IDF value
+            vector[dimension] *= math.log(
                 len(listOfVectors) / DF(dimension, listOfVectors)
             )
 
 
-# the following method will go over all the XML documents, transform them into tree,
-# and then get the vector of TF of all of them
+# Function to get vectors for all XML files provided in the list
 def getAllVectors(XMLpaths):
-    listOfVectors = {}
-    for xmlfile in XMLpaths:
-        print(xmlfile)
-        tree = ET.parse(xmlfile)
-        root = tree.getroot()
-        vector = {}
-        getVectorWithTF(root, "", vector)
-        listOfVectors[xmlfile] = vector
+    listOfVectors = {}  # Initializing an empty dictionary to store vectors
+    for xmlfile in XMLpaths:  # Iterating through each XML file path
+        print(xmlfile)  # Printing the current XML file path
+        tree = ET.parse(xmlfile)  # Parsing the XML file
+        root = tree.getroot()  # Getting the root tag of the XML
+        vector = {}  # Initializing an empty vector
+        getVectorWithTF(
+            root, "", vector
+        )  # Getting the vector with term frequency for the XML data
+        listOfVectors[xmlfile] = vector  # Storing the vector in the dictionary
 
+    # Printing vectors with only term frequency
     print("Vectors with only TF")
     pprint(listOfVectors)
-    IDF(listOfVectors)
+    IDF(listOfVectors)  # Calculating IDF for all vectors
+    # Printing vectors with both term frequency and inverse document frequency
     print("Vectors with TF and IDF")
     pprint(listOfVectors)
-    return listOfVectors
+    return listOfVectors  # Returning the list of vectors
 
-#Example 
 
-XMLpaths=["XMLdocuments1/city1.xml" , "XMLdocuments1/city2.xml"]
-
-getAllVectors(XMLpaths)
-query = getTextQueryVector("Bosphorus Istanbul")
-
-pprint(query)
+# Main execution block
+if __name__ == "__main__":
+    XMLpaths = [
+        "XMLdocuments1/city1.xml",
+        "XMLdocuments1/city2.xml",
+    ]  # List of XML file paths
+    getAllVectors(XMLpaths)  # Getting vectors for all XML files
+    query = getTextQueryVector(
+        "I like to eat apples"
+    )  # Getting the vector for a text query
+    pprint(query)  # Printing the query vector
